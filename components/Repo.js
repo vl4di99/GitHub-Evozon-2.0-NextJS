827712a85187b66eae6d9e0a1fc7f621f411ecde
Lucia Avanu <avanulucia.miruna@yahoo.com>
2022-08-18 11:01:26 +0300

    added clickHandler w/ routing for DETAILS btn

--- components/Repo.js
+++ components/Repo.js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -5,19 +6,27 @@ import { Button, Typography, CircularProgress } from "@mui/material";
 import Languages from "./LanguagesRatio/Languages";
 
 export const Repo = ({ repo }) => {
+  const router = useRouter();
+
+  const clickHandler = () => {
+    router.push(`/${user}/${repo.name}`);
+  };
+
   return (
     <Card
       className="mt-4 border-0 bg-slate-100 drop-shadow-md"
       variant="outlined"
     >
       <CardContent className="p-0 px-2">
-        <Typography color="#455a64" className="text-xl font-semibold mb-4 p-2">
+        <Typography color="#3B81F6" className="text-xl font-semibold mb-4 p-2">
           {repo.name}
         </Typography>
         <Languages repo={repo} />
       </CardContent>
       <CardActions>
-        <Button size="small">details</Button>
+        <Button size="small" onClick={clickHandler}>
+          details
+        </Button>
       </CardActions>
     </Card>
   );
--- components/ReposList.js
+++ components/ReposList.js
@@ -1,10 +1,20 @@
+import { useState, useEffect } from "react";
 import { reposArr } from "../pages/mockRepos";
 import { Repo } from "./Repo";
 
-export const ReposList = () => {
+export const ReposList = ({ filterBy }) => {
+  const [reposToShow, setReposToShow] = useState(reposArr);
+
+  useEffect(() => {
+    const newArr = reposArr.filter((repo) =>
+      repo.name.toLowerCase().includes(filterBy.toLowerCase())
+    );
+    setReposToShow(newArr);
+  }, [filterBy]);
+
   return (
     <div className="repos-container">
-      {reposArr.map((repo) => {
+      {reposToShow.map((repo) => {
         return <Repo repo={repo} key={repo.id} />;
       })}
     </div>

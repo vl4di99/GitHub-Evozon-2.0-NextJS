1a41c962c0eefc622581c2097453c815ebeaca1c
Dionisie-Vladut Lorincz <dionisie.lorincz@yahoo.com>
2022-08-23 15:05:54 +0300

    Changed repoName headers

--- components/Premium/RepositoryInfo.js
+++ components/Premium/RepositoryInfo.js
@@ -10,6 +10,7 @@ import {
   Paper,
   Typography,
 } from "@mui/material";
+import HistoryIcon from "@mui/icons-material/History";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Container } from "@mui/system";
 import axios from "axios";
@@ -22,6 +23,13 @@ import RepoData from "./RepoData";
 function RepositoryInfo({ avatar, name, content, commits }) {
   const repo = useRecoilValue(repoInfo);
   const [, setRepoCommits] = useState([]);
+  const currentDate = new Date();
+  const commitDate = new Date(commits[0]?.commit.committer.date);
+  const difference_Bet_Dates = Math.ceil(
+    (currentDate.getTime() - commitDate.getTime()) / (1000 * 3600 * 24)
+  );
+
+  console.log(commits);
 
   useEffect(() => {
     setRepoCommits(commits);
@@ -46,17 +54,60 @@ function RepositoryInfo({ avatar, name, content, commits }) {
               },
             })}
           >
-            <Container maxWidth={false}>
+            <Container>
               <Paper elevation={10}>
-                <ListItem component="div" disablePadding>
-                  <ListItemButton sx={{ height: 56 }}>
-                    <ListItemText
-                      primary="Repo Content"
-                      primaryTypographyProps={{
-                        fontWeight: "medium",
-                        variant: "body2",
-                      }}
-                    />
+                <ListItem
+                  component="div"
+                  disablePadding
+                  className="flex flex-row min-w-full items-center"
+                >
+                  <ListItemButton
+                    sx={{ height: 56 }}
+                    className="flex flex-row min-w-full justify-between cursor-default"
+                  >
+                    <Box className="flex flex-row items-center justify-center">
+                      <Avatar
+                        src={commits[0]?.committer.avatar_url}
+                        alt="Committer image"
+                        className="w-7 h-7"
+                      />
+                      <Typography variant="h7" className="hover:underline m-3">
+                        <b>{commits[0]?.committer.login}</b>
+                      </Typography>
+                      <Typography
+                        variant="subtitle2"
+                        className="hover:underline hover:text-blue-700 m-2"
+                      >
+                        <a href={commits[0]?.html_url}>
+                          {commits[0]?.commit.message}
+                        </a>
+                      </Typography>
+                    </Box>
+                    <Box className="flex flex-row items-center justify-center">
+                      <Typography
+                        variant="subtitle2"
+                        className="flex hover:text-blue-700 m-2"
+                      >
+                        <a href={commits[0]?.html_url}>
+                          <u>{commits[0]?.sha.slice(0, 7)}</u>
+                        </a>
+                      </Typography>
+                      <Typography
+                        variant="subtitle2"
+                        className="flex hover:text-blue-700 hover:underline m-2"
+                      >
+                        <a href={commits[0]?.html_url}>
+                          <b>{difference_Bet_Dates}</b>&nbsp;days ago
+                        </a>
+                      </Typography>
+                      <Typography
+                        variant="subtitle2"
+                        className="flex hover:text-blue-700"
+                      >
+                        <HistoryIcon className="mr-1" />
+                        <b>{commits.length} </b>&nbsp;commits
+                      </Typography>
+                    </Box>
                   </ListItemButton>
                 </ListItem>
                 <Divider />

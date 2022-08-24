c53a15802fa70121d715da76253f4dfa052e0a0c
Dionisie-Vladut Lorincz <dionisie.lorincz@yahoo.com>
2022-08-24 10:57:49 +0300

    Redesigned redirect pages

--- pages/successLogin.js
+++ pages/successLogin.js
@@ -1,9 +1,11 @@
-import { Typography } from "@mui/material";
+import { Avatar, Box, Typography } from "@mui/material";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { axiosHeaders } from "../atoms/repository";
+import straightRightArrow from "../images/straight-right-arrow.png";
 
 function SuccessLogin() {
   const [seconds, setSeconds] = useState(3);
@@ -26,10 +28,25 @@ function SuccessLogin() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <Typography variant="h2">
-        Hi,{session?.name}! <br /> You have successfully logged in and will be
-        redirected in {seconds} seconds
-      </Typography>
+      <Box className="flex flex-col items-center justify-center w-1/2 h-1/2 shadow-2xl">
+        <Box className="w-2/6 h-2/6">
+          <Image
+            src={straightRightArrow}
+            alt="Redirect img"
+            objectFit="cover"
+          />
+        </Box>
+        <Typography variant="h3" className="m-10">
+          Hi,{session?.name}!
+        </Typography>
+        <Typography variant="h5" className="m-3">
+          You have successfully logged in and will be redirected in {seconds}{" "}
+          seconds
+        </Typography>
+        <Box className="flex self-end bg-gradient-to-r from-cyan-300 to-sky-700 h-2/6 w-full">
+          &nbsp;
+        </Box>
+      </Box>
     </div>
   );
 }
--- pages/successLogout.js
+++ pages/successLogout.js
@@ -1,7 +1,10 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 import { useEffect, useState } from "react";
+
+import uturnSign from "../images/uturn.png";
 
 function SuccessLogout() {
   const [seconds, setSeconds] = useState(3);
@@ -19,10 +22,18 @@ function SuccessLogout() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <Typography variant="h2">
-        You have successfully logged out and will be redirected in {seconds}{" "}
-        seconds
-      </Typography>
+      <Box className="flex flex-col items-center justify-center w-1/2 h-1/2 shadow-2xl">
+        <Box className="w-2/6 h-2/6">
+          <Image src={uturnSign} alt="Redirect img" objectFit="cover" />
+        </Box>
+        <Typography variant="h5" className="m-3">
+          You have successfully logged out and will be redirected in {seconds}{" "}
+          seconds
+        </Typography>
+        <Box className="flex self-end bg-gradient-to-r from-rose-900 to-red-400 h-2/6 w-full">
+          &nbsp;
+        </Box>
+      </Box>
     </div>
   );
 }
